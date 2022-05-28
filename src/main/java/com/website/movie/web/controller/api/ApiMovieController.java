4a55ff8848d31e1717cff0ebf90842fc961b90ae
JangWonSeob <jqk9807@gmail.com>
2022-05-28 21:31:45 +0900

    axios 샘플

---
+++ src/main/java/com/website/movie/web/controller/api/ApiMovieController.java
@@ -0,0 +1,63 @@
+package com.website.movie.web.controller.api;
+
+import com.website.movie.biz.dto.MovieDto;
+import com.website.movie.biz.model.JsonResult;
+import com.website.movie.biz.service.BoardService;
+import com.website.movie.biz.service.MovieService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ApiMovieController {
+
+    private final BoardService boardService;
+    private final MovieService movieService;
+
+
+    @PostMapping("/api/movie/get.api")
+    @ApiOperation(value = "영화 한 건 조회 API", notes = "영화 한 건에 대해서 조회 가능합니다.")
+    public JsonResult get(@RequestBody MovieDto parameter) {
+
+        MovieDto result = movieService.get(parameter);
+
+        if(result == null) {
+            return JsonResult.fail(" 입력 값이 옳지 않습니다. ");
+        }
+
+        return JsonResult.success(result);
+    }
+
+    @PostMapping("/api/movie/gets.api")
+    @ApiOperation(value = "영화 리스트 조회 API", notes = "영화 리스트 조회가 가능합니다.")
+    public JsonResult gets(@RequestBody MovieDto parameter) {
+
+        parameter.initPage();
+
+        List<MovieDto> list = movieService.gets(parameter);
+        int totalCount = movieService.totalCount(parameter);
+
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("list", list);
+        result.put("totalCount", totalCount);
+
+        return JsonResult.success(result);
+    }
+
+//    @PostMapping("/api/movie/viewcount/add.api")
+//    @ApiOperation(value = "영화 조회수 증가 API", notes = "영화 조회수를 증가시킵니다.")
+//    public void viewCountUp(@RequestBody BoardDto model) {
+//
+//        boardService.viewCountUp(model);
+//    }
+
+
+}
---
+++ src/main/resources/static/js/api.js
@@ -0,0 +1,44 @@
+if (typeof (movie) == 'undefined') { movie = {}; }
+if (typeof (movie.app) == 'undefined') { movie.app = {}; }
+if (typeof (movie.app.project) == 'undefined') { movie.app.project = {}; }
+
+
+(function (_context_) {
+
+    let responseCallBack = function (response) {
+        response = response || {}
+        response.data = response.data || {};
+
+        response.data.header = response.data.header || {};
+        response.data.header.message = response.data.header.message || '';
+        response.data.header.result = response.data.header.result || false;
+        if (response.data.header.result !== true) {
+            response.data.header.result = false;
+        }
+
+        console.log(response);
+        return response;
+    };
+
+    let postFunction = function (url, parameter, callback) {
+        callback = callback || (function () {
+        });
+
+        axios.post(url, parameter).then(function (response) {
+            response = responseCallBack(response);
+            callback(response.data.header.result, response.data.header.message, response.data.body.data);
+        }).catch(function (err) {
+            console.log(err);
+        })
+    };
+
+    _context_.post = function(url, param, callback) {
+        postFunction(url, param, callback);
+    };
+
+
+})(movie.app.project);
+
+
+if (typeof (API_CALL) == 'undefined') { API_CALL = {}; }
+API_CALL = movie.app.project;
