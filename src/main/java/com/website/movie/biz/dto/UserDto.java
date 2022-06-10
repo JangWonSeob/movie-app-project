d0d4b83460639236d1c13a9f5ba25745d5753d39
김민수 <minsu980107@naver.com>
2022-06-10 21:33:51 +0900

    유효성검사 구현중

--- src/main/java/com/website/movie/biz/dto/UserDto.java
+++ src/main/java/com/website/movie/biz/dto/UserDto.java
@@ -28,8 +28,8 @@ public class UserDto extends BaseDto implements UserDetails {
     private String nickname;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    @Pattern(regexp="^[a-zA-Z0-9]{8,20}$",
+            message = "비밀번호는 영문 대,소문자와 숫자 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
     private String certified;
     private Collection<? extends GrantedAuthority> authorities;  //두개이상의 권한을 리스트에 담아 사용
