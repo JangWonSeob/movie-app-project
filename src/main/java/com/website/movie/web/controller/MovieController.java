eccfe5e3516e800ef4f9d744bc75d1ed7ad64ac5
rlaalstn0107 <minsu980107@naver.com>
2022-11-18 18:17:48 +0900

    영화검색구현중

--- src/main/java/com/website/movie/web/controller/MovieController.java
+++ src/main/java/com/website/movie/web/controller/MovieController.java
@@ -66,9 +66,21 @@ public class MovieController {
     }
 
     @GetMapping("/movie/search")
-    public String search(Model model) {
+    public String search(Model model,CodeDto parameter) {
 
-        model.addAttribute("codeList", movieService.main());
+        if(parameter.getSubId() == null) {
+            parameter.setSubId("12");
+        }
+
+        CodeDto codeDto = codeService.getBySubId(parameter);
+
+        if(codeDto != null) {
+            // 값이 없으면 어드벤처로 세팅
+            parameter.setSubId("12");
+            codeDto = codeService.getBySubId(parameter);
+        }
+
+        model.addAttribute("code", codeDto);
 
         return "movie/search";
     }
---
+++ src/main/resources/static/js/movie/search.js
@@ -0,0 +1,88 @@
+let PAGE_INDEX = 1;
+// TODO: 페이지 사이즈 수정
+let PAGE_SIZE = 8;
+let SEARCH_GENRE = '';
+
+let LIST = [];
+let TOTAL_COUNT = 0;
+
+var getGenre = function () {
+
+    let queryString = location.search;
+
+    if(queryString != undefined && queryString != '') {
+        queryString = queryString.replaceAll('?', '');
+        queryString = queryString.split("&");
+        for (let i = 0; i < queryString.length; i++) {
+            if ((queryString[i].indexOf('subId') + queryString[i].indexOf('='))  > 1) {
+                SEARCH_GENRE = queryString[i].split('=')[1];
+                break;
+            }
+        }
+    }
+}
+
+var getList = function() {
+    const url = '/api/movie/gets.api';
+    const param = {
+        pageIndex: PAGE_INDEX,
+        pageSize: PAGE_SIZE,
+        searchGenre: SEARCH_GENRE,
+    };
+
+    API_CALL.post(url, param, function (result, message, data) {
+        if(!result) {
+            alert(message);
+            return false;
+        }
+        let movieList = data.list || [];
+        TOTAL_COUNT = data.totalCount || 0;
+
+        for (let i = 0; i < movieList.length; i++) {
+
+            // <div className="col-lg-2">
+            //     <div className="card" style="width: 15rem;">
+            //         <!--                    <img src="test/test.png" class="card-img-top" alt="...">-->
+            //     </div>
+            //     <div>제목</div>
+            // </div>
+
+            // <div th:each="movie : ${code.movieList}" className="col-lg-2">
+            //     <a th:href="@{/movie/detail/{id}(id=${movie.id})}">
+            //         <div className="card" style="width: 15rem;">
+            //             <img th:src="${movie.fullPosterPath}" className="card-img-top" alt="...">
+            //         </div>
+            //         <div style="color: white" th:text="${movie.title}"></div>
+            //     </a>
+            // </div>
+
+            let append = '';
+            append += '<div class="col-lg-3 mb-4">';
+            append += '<a class="text-center text-decoration-none" href=" /movie/detail/' + movieList[i].id + ' ">';
+            append += '<div class="card">';
+            append += '<img src=" ' + movieList[i].fullPosterPath +  ' " class="card-img-top" alt="...">';
+            append += '</div>';
+            append += '<div style="color: white;">' + movieList[i].title + '</div>';
+            append += '</a>';
+            append += '</div>';
+
+            $('#movieList').append(append);
+        }
+    })
+}
+
+$(function(){
+    getGenre();
+    getList();
+
+    $('#moreList').on('click', function () {
+        PAGE_INDEX += 1;
+
+        if ((PAGE_INDEX - 1) * PAGE_SIZE > TOTAL_COUNT) {
+            alert('더 이상 데이터가 없습니다.');
+            return false;
+        }
+
+        getList();
+    })
+});
